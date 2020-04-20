46c2baa327d9e77de3f47d3deacd4cc8f6a0a892
ajaojnr5 <abdussamadajao@gmail.com>
2020-04-20 01:03:45 +0100

    Updates

---
+++ precache-manifest.a8b7237150bffa1f98b2de03680f59df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c598f32909f54e1bf8282c8441f0d75e",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "718f936c8cf4a82ea9ee",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "382f626f0c6bc9232ed7",
+    "url": "/contactmanager/static/js/2.a6fc9c6d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.a6fc9c6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "718f936c8cf4a82ea9ee",
+    "url": "/contactmanager/static/js/main.981c7023.chunk.js"
+  },
+  {
+    "revision": "cc9bd95ebc68c1f7c6c1",
+    "url": "/contactmanager/static/js/runtime-main.ea2785c0.js"
+  }
+]);
